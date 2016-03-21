b1e4363f3056e043a20a457d9f7731bb2a270e99
dimitridarras <ddarras2012@gmail.com>
2016-03-21 15:03:46 -0400

    added some test js and online resources

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -1,12 +1,22 @@
 Drupal.behaviors.myModule = {
   attach : function()
   {
-    alert("made it");
-    alert(Drupal.settings.webspeech_tts.proof_of_concept);
+    //alert("made it");
+    //alert(Drupal.settings.webspeech_tts.proof_of_concept);
     render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
     //var msg = new SpeechSynthesisUtterance('Hello World');
     var msg = new SpeechSynthesisUtterance(render_voice);
     window.speechSynthesis.speak(msg);
+
+
+    //speechSynthesis.getVoices().forEach(function(voice) {
+    //console.log(voice.name, voice.default ? '(default)' :'');
+
+var msg = new SpeechSynthesisUtterance('I see dead people!');
+msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Whisper'; })[0];
+speechSynthesis.speak(msg);
+
+
 
       }
 }
