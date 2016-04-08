a49d788b9104e381778b7ad292c8484f81deebb6
dimitridarras <ddarras@gmu.edu>
2016-04-08 16:48:48 -0400

    updates

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -3,9 +3,20 @@
   Drupal.behaviors.webspeech_tts = {
     attach: function () {
       $('.form-submit').click(function () {
+        // Chrome loads voices asynchronously.
+        window.speechSynthesis.onvoiceschanged = function(e) {
+        loadVoices();
+        };
+
         render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
         var msg = new SpeechSynthesisUtterance(render_voice);
+       // http://jsbin.com/gosaqihi/9/edit?js,console
+       // msg.lang = 'ja-JP'; 
+       msg.voice = 'Fiona';
         window.speechSynthesis.speak(msg);
+
+
+
       });
     }
   }
