7743288a82c881e311ebafe3efdfa4f2cd55fc5d
dimitridarras <ddarras@gmu.edu>
2016-04-08 14:12:53 -0400

    cleaned up files, basic working prototype

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -1,20 +1,13 @@
 
-
 (function ($) {
-//alert ("foo");
-
   Drupal.behaviors.webspeech_tts = {
     attach: function () {
-      //alert("in behavior");
       $('.form-submit').click(function () {
-        alert('button clicked');
         render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
         var msg = new SpeechSynthesisUtterance(render_voice);
         window.speechSynthesis.speak(msg);
       });
     }
-
-
   }
 
 }(jQuery))
