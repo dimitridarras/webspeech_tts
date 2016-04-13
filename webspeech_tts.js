26b58fa7fcdad727822debdf368780283e8d6afc
dimitridarras <ddarras@gmu.edu>
2016-04-13 19:04:15 -0400

    added config to voice var, need to wire up module node toggle & display

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -15,9 +15,17 @@
         var render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
     
         var msg = new SpeechSynthesisUtterance(render_voice);
+        var the_lang = Drupal.settings.webspeech_tts.the_lang;
+
         //replace name and array value with form vars
-        msg.voice = voices.filter(function(voice) {return voice.name = 'Agnes'})[2];
-        console.log(msg.voice);
+        //msg.voice = voices.filter(function(voice) {return voice.name = 'Agnes'})[2];
+        msg.lang = the_lang;
+
+        console.log(msg.voice + " This is the voice!");
+        console.log(msg.lang + ": This is the accent!");
+
+
+ 
      
         window.speechSynthesis.speak(msg);
 
