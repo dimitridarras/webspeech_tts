01aebd250a7b40b53bc5e77acbbeb062caee2381
dimitridarras <ddarras@gmu.edu>
2016-04-14 16:52:40 -0400

    updates to config and vars - known issues, may cull back

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -9,23 +9,44 @@
       //console.log(voices);
 
       $('.form-submit').click(function () {
-        // Chrome loads voices asynchronously.
+        //Chrome loads voices asynchronously.
         //console.log(voices);
 
         var render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
     
         var msg = new SpeechSynthesisUtterance(render_voice);
-        var the_lang = Drupal.settings.webspeech_tts.the_lang;
+        var the_accent = Drupal.settings.webspeech_tts.tts_accent_js;
+        var the_voice = Drupal.settings.webspeech_tts.tts_voices_js;
+        var the_translated_lang = Drupal.settings.webspeech_tts.tts_translated_lang_js;
+        var the_volume = Drupal.settings.webspeech_tts.tts_volume_js;
+        var the_rate = Drupal.settings.webspeech_tts.tts_rate_js;
+        var the_pitch = Drupal.settings.webspeech_tts.tts_pitch_js;                                        
 
         //replace name and array value with form vars
         //msg.voice = voices.filter(function(voice) {return voice.name = 'Agnes'})[2];
-        msg.lang = the_lang;
+        
 
-        console.log(msg.voice + " This is the voice!");
-        console.log(msg.lang + ": This is the accent!");
+        if(the_translated_lang)
+        {
+          msg.lang = the_translated_lang;
+        }
 
+        else {
+          msg.lang = the_accent;       
+        }
 
- 
+        msg.voice = the_voice;
+        msg.volume = the_volume;
+        msg.rate = the_rate;
+        msg.pitch = the_pitch;
+
+        console.log(msg.the_accent + ": This is the accent!");
+        console.log(msg.the_accent + ": This is the translated language!");        
+        console.log(msg.the_voice + " This is the voice!");
+        console.log(msg.the_translated_lang + " This is the voice!");
+        console.log(msg.the_volume + ": This is the Language!");        
+        console.log(msg.the_rate + " This is the voice!");
+        console.log(msg.the_pitch + ": This is the Language!");
      
         window.speechSynthesis.speak(msg);
 
