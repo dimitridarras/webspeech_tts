3d9c6f25234f47f51c3e34e5a43f7ab40877f7b7
dimitridarras <ddarras@gmu.edu>
2016-04-15 16:08:48 -0400

    exception for voice

--- webspeech_tts.js
+++ webspeech_tts.js
@@ -12,11 +12,16 @@
         //Chrome loads voices asynchronously.
         //console.log(voices);
 
+        var js_voices = speechSynthesis.getVoices();
+        console.log(js_voices);
+        //This is a special case above for voices - see msg.voice below
+
         var render_voice = Drupal.settings.webspeech_tts.proof_of_concept;
     
         var msg = new SpeechSynthesisUtterance(render_voice);
         var the_lang_accent = Drupal.settings.webspeech_tts.tts_lang_accent_js;
         var the_voice = Drupal.settings.webspeech_tts.tts_voice_js;
+
    
         var the_volume = Drupal.settings.webspeech_tts.tts_volume_js;
         var the_rate = Drupal.settings.webspeech_tts.tts_rate_js;
@@ -24,13 +29,13 @@
 
 
         msg.lang = the_lang_accent;
-        msg.voice = the_voice;
+        msg.voice = js_voices.filter(function(js_voices) {return js_voices.name == the_voice;})[0];
         msg.volume = the_volume;
         msg.rate = the_rate;
         msg.pitch = the_pitch;
 
         console.log(msg.lang + ": This is the accent/lang");
-         console.log(msg.voice + ": This is the voice");
+        console.log(msg.voice + ": This is the voice");
         /*console.log(msg.the_accent + ": This is the translated language!");        
         console.log(msg.the_voice + " This is the voice!");
         console.log(msg.the_translated_lang + " This is the voice!");
