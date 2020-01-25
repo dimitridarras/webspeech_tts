30f456b3aaa4c810fa6eca8897452c2b36a68d4c
Dimitri Darras <ddarras2012@gmail.com>
2020-01-25 01:40:04 -0500

    Updating github repo with latest code from Drupal.org.

--- webspeech_tts-block.tpl.php
+++ webspeech_tts-block.tpl.php
@@ -2,21 +2,15 @@
 
 /**
  * @file
- *   Themes the Webspeech TTS block - adds a JS Sniffer	for WebSpeech Support
+ * Themes the Webspeech TTS block - adds a JS Sniffer for WebSpeech Support.
  */
-
 ?>
-
 <div id='webspeech_tts-rocks'>
-
-<script>
-if (window.SpeechSynthesisUtterance === undefined) {
-	document.write("<div class='messages error'>Your Browser Does Not Support the WebSpeechAPI.  Currenly, only Chrome supports it and Safari, with limited support.<br/></br></div>");
-}
-</script>
-
-
-
+  <script>
+    if (window.SpeechSynthesisUtterance === undefined) {
+      document.write("<div class='messages error'>Your Browser Does Not Support the WebSpeechAPI.  Currently, only Chrome supports it and Safari, with limited support.<br/></br></div>");
+    }
+  </script>
   <?php print t('Use the controls below for web speech: <br/> <br/>'); ?>
   <?php print $form; ?>
 </div>
